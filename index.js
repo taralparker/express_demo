abaa1ad0535e605eed6b2f4c2d1a906805ac8adc
Tara <teepee@gmail.com>
2020-07-11 15:04:01 -0700

    create express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/secret/:password', function(req, res)  {
+    if (req.params.password !== 'asdfasdf'){
+        return res.status(403).send('You are dumbbbb.')
+    }
+    res.send('Hello World!'+Math.random())
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
