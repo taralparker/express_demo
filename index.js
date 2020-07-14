c2eb96cdb75a39408a37ae5b96766f1f1aa75da5
Tara <teepee@gmail.com>
2020-07-14 00:26:07 -0700

    formatting

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/secret/:password', function(req, res)  {
-    if (req.params.password !== 'asdfasdf'){
-        return res.status(403).send('You are dumbbbb.')
-    }
-    res.send('Hello World!'+Math.random())
+app.get('/secret/:password', function (req, res) {
+  if (req.params.password !== 'asdfasdf') {
+    return res.status(403).send('You are dumbbbb.')
+  }
+
+  res.send('Hello World!' + Math.random())
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', function (req, res) {
+  res.send('This is the home route!')
+})
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
